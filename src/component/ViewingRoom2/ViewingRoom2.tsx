114b311fde5dd382499ef872b9197c3bce3e6277
Kay Kim <kimjh@bawi.org>
2020-06-13 00:39:55 +0900

    lantern

--- src/component/ViewingRoom2/ViewingRoom2.tsx
+++ src/component/ViewingRoom2/ViewingRoom2.tsx
@@ -8,26 +8,26 @@ import useWindowSize from '../useWindowSize';
 // const STORAGE_URL_MD = 'https://d3upf6md31d3of.cloudfront.net';
 
 export default function ViewingRoom({ idx, src }: { idx: number, src: string }) {
-  const [isLandscape, setLandscape] = React.useState<boolean | null>(null);
+  const [isLandscape, setLandscape] = React.useState<boolean[] | null>(null);
   const [maxSize, setMaxSize] = React.useState<string[]>(['calc(80% - 120px)', 'calc(80% - 200px)']);
   const refContainer = React.useRef<HTMLDivElement | null>(null);
 
   const [innerWidth, innerHeight] = useWindowSize();
+  const imageRatio = info[idx].width / info[idx].height;
 
   React.useEffect(() => {
-    const imageRatio = info[idx].width / info[idx].height;
     const windowRatio = innerWidth / innerHeight;
 
-    setLandscape(imageRatio > windowRatio);
-  }, [idx, innerWidth, innerHeight]);
+    setLandscape([imageRatio > windowRatio, imageRatio > 1.5]);
+  }, [imageRatio, innerWidth, innerHeight]);
 
   React.useEffect(() => {
     if (innerWidth < 500) {
       setMaxSize(['calc(100% - 120px)', 'calc(100% - 200px)']);
-    } else if (innerWidth < 1000) {
-      setMaxSize(['calc(90% - 120px)', 'calc(90% - 200px)']);
+    } else if (innerWidth < 1500) {
+      setMaxSize(['calc(75% - 120px)', 'calc(75% - 200px)']);
     } else {
-      setMaxSize(['calc(80% - 120px)', 'calc(80% - 200px)']);
+      setMaxSize(['calc(60% - 120px)', 'calc(60% - 200px)']);
     }
   }, [innerWidth]);
 
@@ -41,12 +41,53 @@ export default function ViewingRoom({ idx, src }: { idx: number, src: string }) 
         className="maximumContainer"
       >
         {isLandscape !== null && (
-          <img
-            alt={`Decorum ${info[idx].year} - ${info[idx].id}`}
-            src={src}
-            width={isLandscape ? '100%' : 'auto'}
-            height={isLandscape ? 'auto' : '100%'}
-          />
+          <>
+            {isLandscape[1] ? (
+              <>
+                <img
+                  alt="lantern"
+                  src={`${process.env.PUBLIC_URL}/lantern (1).png`}
+                  width={isLandscape[0] ? '200%' : 'auto'}
+                  height={isLandscape[0] ? 'auto' : `${180 * imageRatio}%`}
+                  className="lantern"
+                  style={{
+                    left: '25%',
+                    transform: 'translate(-25%, -50%)',
+                  }}
+                />
+                <img
+                  alt="lantern"
+                  src={`${process.env.PUBLIC_URL}/lantern (1).png`}
+                  width={isLandscape[0] ? '200%' : 'auto'}
+                  height={isLandscape[0] ? 'auto' : `${180 * imageRatio}%`}
+                  className="lantern"
+                  style={{
+                    right: '25%',
+                    transform: 'translate(25%, -50%)',
+                  }}
+                />
+              </>
+            ) : (
+              <img
+                alt="lantern"
+                src={`${process.env.PUBLIC_URL}/lantern (1).png`}
+                width={isLandscape[0] ? '200%' : 'auto'}
+                height={isLandscape[0] ? 'auto' : `${180 * imageRatio}%`}
+                className="lantern"
+                style={{
+                  left: '50%',
+                  transform: 'translate(-50%, -50%)',
+                }}
+              />
+            )}
+            <img
+              alt={`Decorum ${info[idx].year} - ${info[idx].id}`}
+              src={src}
+              width={isLandscape[0] ? '100%' : 'auto'}
+              height={isLandscape[0] ? 'auto' : '100%'}
+              className="painting"
+            />
+          </>
         )}
       </div>
     </div>
