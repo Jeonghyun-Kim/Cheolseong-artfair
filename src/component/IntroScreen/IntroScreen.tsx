7538080f6cc240b128d46bd016b8105ae69367ad
Kay Kim <kimjh@bawi.org>
2020-06-17 20:24:53 +0900

    change timer to 2s

--- src/component/IntroScreen/IntroScreen.tsx
+++ src/component/IntroScreen/IntroScreen.tsx
@@ -7,7 +7,7 @@ import useWindowSize from '../useWindowSize';
 const STORAGE_URL_MD = 'https://d3upf6md31d3of.cloudfront.net';
 const REWRITE_URL = 'https://www.notion.so/3f9ecec2a1d940c1b001a22b973b0794?v=b6dd3ece14c44efd9ec9b0a90fee226e';
 
-const timer = 1.5;
+const timer = 2;
 
 export default function IntroScreen() {
   const [innerWidth] = useWindowSize();
