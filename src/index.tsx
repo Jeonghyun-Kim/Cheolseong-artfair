2e448141dbc063b32addf2a0cf15c214330fe39f
Kay Kim <kimjh@bawi.org>
2020-06-18 14:38:27 +0900

    .

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/stable';
 
 import './index.scss';
 import AppRouter from './AppRouter';
