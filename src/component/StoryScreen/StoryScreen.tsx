4cdc682824801a8837a54a1ab087defa86d2c6d5
Kay Kim <kimjh@bawi.org>
2020-06-25 15:15:39 +0900

    add scroll to top

--- src/component/StoryScreen/StoryScreen.tsx
+++ src/component/StoryScreen/StoryScreen.tsx
@@ -3,11 +3,16 @@ import { useHistory } from 'react-router-dom';
 import YouTube, { Options } from 'react-youtube';
 import IconButton from '@material-ui/core/IconButton';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import UpIcon from '@material-ui/icons/ArrowUpward';
 
 import './StoryScreen.scss';
 
 export default function StoryScreen() {
   const history = useHistory();
+
+  const handleScrollToTop = () => {
+    window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+  };
 
   const videoOpts: Options = {
     width: '60%',
@@ -20,10 +25,15 @@ export default function StoryScreen() {
     <div className="App unselectable">
       <IconButton
         id="backIcon"
-        className="fixed"
         onClick={() => history.push('/')}
       >
         <ArrowBackIcon fontSize="large" />
+      </IconButton>
+      <IconButton
+        id="upIcon"
+        onClick={() => handleScrollToTop()}
+      >
+        <UpIcon fontSize="small" />
       </IconButton>
       <div className="storyContainer">
         <div id="videoPlayer">
@@ -32,6 +42,8 @@ export default function StoryScreen() {
         <div className="quote">
           <img alt="quote" src={`${process.env.PUBLIC_URL}/letters/quote.png`} />
         </div>
+
+
       </div>
     </div>
   );
