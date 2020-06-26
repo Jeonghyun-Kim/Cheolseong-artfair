2acd676c61bc0dcc367d6eba604db22d9777d783
Kay Kim <kimjh@bawi.org>
2020-06-26 10:59:09 +0900

    remove infinityscroll

--- src/component/ItemList/ItemList.tsx
+++ src/component/ItemList/ItemList.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React from 'react';
-import InfiniteScroll from 'react-infinite-scroll-component';
+// import InfiniteScroll from 'react-infinite-scroll-component';
 import { useHistory } from 'react-router-dom';
 
 import './ItemList.scss';
@@ -14,31 +14,31 @@ const imageSize = 350;
 const margin = [30, 80];
 const breakRatio = 1.5;
 
-const NUM_PAGE = 5;
+// const NUM_PAGE = 5;
 
 export default function ItemList({ indexMap }:{
   indexMap: number[],
 }) {
-  const [items, setItems] = React.useState<number[]>(
-    sessionStorage.getItem('@items')
-      ? JSON.parse(sessionStorage.getItem('@items') as string)
-      : indexMap.slice(0, Math.min(NUM_PAGE, indexMap.length) - 1),
-  );
-  const [hasMore, setHasMore] = React.useState<boolean>(NUM_PAGE < indexMap.length);
+  // const [items, setItems] = React.useState<number[]>(
+  //   sessionStorage.getItem('@items')
+  //     ? JSON.parse(sessionStorage.getItem('@items') as string)
+  //     : indexMap.slice(0, Math.min(NUM_PAGE, indexMap.length) - 1),
+  // );
+  // const [hasMore, setHasMore] = React.useState<boolean>(NUM_PAGE < indexMap.length);
   const [innerWidth] = useWindowSize();
 
   const history = useHistory();
 
-  React.useEffect(() => {
-    if (items.length === indexMap.length) {
-      setHasMore(false);
-    }
-  }, [items, indexMap]);
+  // React.useEffect(() => {
+  //   if (items.length === indexMap.length) {
+  //     setHasMore(false);
+  //   }
+  // }, [items, indexMap]);
 
-  React.useEffect(() => {
-    setItems(indexMap.slice(0, Math.min(NUM_PAGE, indexMap.length) - 1));
-    sessionStorage.removeItem('@items');
-  }, [indexMap]);
+  // React.useEffect(() => {
+  //   setItems(indexMap.slice(0, Math.min(NUM_PAGE, indexMap.length) - 1));
+  //   sessionStorage.removeItem('@items');
+  // }, [indexMap]);
 
   // React.useEffect(() => {
   //   const storedItems = sessionStorage.getItem('@items');
@@ -52,122 +52,122 @@ export default function ItemList({ indexMap }:{
     setTimeout(() => history.push(`/viewing-room/${value}`), 10);
   };
 
-  const getMoreData = () => {
-    if (items.length === indexMap.length) {
-      setHasMore(false);
-      return;
-    }
+  // const getMoreData = () => {
+  //   if (items.length === indexMap.length) {
+  //     setHasMore(false);
+  //     return;
+  //   }
 
-    setTimeout(() => {
-      setItems(
-        items.concat(
-          indexMap.slice(items.length, Math.min(items.length + NUM_PAGE, indexMap.length)),
-        ),
-      );
-      sessionStorage.setItem('@items', JSON.stringify(items));
-    }, 0);
-  };
+  //   setTimeout(() => {
+  //     setItems(
+  //       items.concat(
+  //         indexMap.slice(items.length, Math.min(items.length + NUM_PAGE, indexMap.length)),
+  //       ),
+  //     );
+  //     sessionStorage.setItem('@items', JSON.stringify(items));
+  //   }, 0);
+  // };
 
-  const Loader = () => (
-    <div
-      style={{ width: '100%', height: '150px', position: 'relative' }}
-    >
-      <img
-        alt="spinner"
-        src={`${process.env.PUBLIC_URL}/Spinner.svg`}
-        id="loadingIcon"
-        width="100px"
-      />
-    </div>
-  );
+  // const Loader = () => (
+  //   <div
+  //     style={{ width: '100%', height: '150px', position: 'relative' }}
+  //   >
+  //     <img
+  //       alt="spinner"
+  //       src={`${process.env.PUBLIC_URL}/Spinner.svg`}
+  //       id="loadingIcon"
+  //       width="100px"
+  //     />
+  //   </div>
+  // );
 
   return (
     <div className="itemContainer unselectable">
-      <InfiniteScroll
+      {/* <InfiniteScroll
         dataLength={items.length}
         next={getMoreData}
         hasMore={hasMore}
         scrollThreshold="50px"
         loader={Loader}
-      >
-        {items.map((value) => {
-          const isSingleLine = innerWidth - 150 < (imageSize + margin[0]) * 2;
-          const imageRatio = info[value].width / info[value].height;
+      > */}
+      {indexMap.map((value) => {
+        const isSingleLine = innerWidth - 150 < (imageSize + margin[0]) * 2;
+        const imageRatio = info[value].width / info[value].height;
 
-          return (
-            <div id="mapping" key={value}>
-              {isSingleLine ? (
-                <div
-                  style={{
-                    width: Math.min(innerWidth - 100, imageSize),
-                    height: 'auto',
-                    margin: `${margin[1] / 2}px ${margin[0] / 2}px`,
+        return (
+          <div id="mapping" key={value}>
+            {isSingleLine ? (
+              <div
+                style={{
+                  width: Math.min(innerWidth - 100, imageSize),
+                  height: 'auto',
+                  margin: `${margin[1] / 2}px ${margin[0] / 2}px`,
+                }}
+              >
+                <img
+                  src={`${STORAGE_URL_XS}/${info[value].src}`}
+                  alt={`${info[value].src.split('.')[0]}`}
+                  width={imageRatio < 1 ? 'auto' : '100%'}
+                  height={imageRatio < 1 ? Math.min(innerWidth - 100, imageSize) : 'auto'}
+                  onClick={() => handleMove(value)}
+                  onKeyDown={(e) => {
+                    if (e.keyCode === 13) {
+                      handleMove(value);
+                    }
                   }}
-                >
+                  className="singleLineListImage"
+                  style={{
+                    borderRadius: info[value].src === '2013_5.gif' ? 999 : 2,
+                  }}
+                />
+              </div>
+            ) : (
+              <div
+                style={{
+                  maxWidth: innerWidth - 100,
+                  width: imageRatio > breakRatio ? (imageSize + margin[0]) * 2 : imageSize,
+                  height: imageSize,
+                  margin: `${margin[1]}px ${margin[0]}px`,
+                }}
+                className="imageBlock"
+              >
+                {imageRatio > breakRatio ? (
                   <img
                     src={`${STORAGE_URL_XS}/${info[value].src}`}
                     alt={`${info[value].src.split('.')[0]}`}
-                    width={imageRatio < 1 ? 'auto' : '100%'}
-                    height={imageRatio < 1 ? Math.min(innerWidth - 100, imageSize) : 'auto'}
+                    width={imageRatio > 2 ? '100%' : 'auto'}
+                    height={imageRatio > 2 ? 'auto' : '100%'}
                     onClick={() => handleMove(value)}
                     onKeyDown={(e) => {
                       if (e.keyCode === 13) {
                         handleMove(value);
                       }
                     }}
-                    className="singleLineListImage"
-                    style={{
-                      borderRadius: info[value].src === '2013_5.gif' ? 999 : 2,
-                    }}
+                    className="listImage"
+                    style={{ borderRadius: info[value].src === '2013_5.gif' ? 999 : 2 }}
                   />
-                </div>
-              ) : (
-                <div
-                  style={{
-                    maxWidth: innerWidth - 100,
-                    width: imageRatio > breakRatio ? (imageSize + margin[0]) * 2 : imageSize,
-                    height: imageSize,
-                    margin: `${margin[1]}px ${margin[0]}px`,
-                  }}
-                  className="imageBlock"
-                >
-                  {imageRatio > breakRatio ? (
-                    <img
-                      src={`${STORAGE_URL_XS}/${info[value].src}`}
-                      alt={`${info[value].src.split('.')[0]}`}
-                      width={imageRatio > 2 ? '100%' : 'auto'}
-                      height={imageRatio > 2 ? 'auto' : '100%'}
-                      onClick={() => handleMove(value)}
-                      onKeyDown={(e) => {
-                        if (e.keyCode === 13) {
-                          handleMove(value);
-                        }
-                      }}
-                      className="listImage"
-                      style={{ borderRadius: info[value].src === '2013_5.gif' ? 999 : 2 }}
-                    />
-                  ) : (
-                    <img
-                      src={`${STORAGE_URL_XS}/${info[value].src}`}
-                      alt={`${info[value].src.split('.')[0]}`}
-                      width={imageRatio > 1 ? '100%' : 'auto'}
-                      height={imageRatio > 1 ? 'auto' : '100%'}
-                      onClick={() => handleMove(value)}
-                      onKeyDown={(e) => {
-                        if (e.keyCode === 13) {
-                          handleMove(value);
-                        }
-                      }}
-                      className="listImage"
-                      style={{ borderRadius: info[value].src === '2013_5.gif' ? 999 : 2 }}
-                    />
-                  )}
-                </div>
-              )}
-            </div>
-          );
-        })}
-      </InfiniteScroll>
+                ) : (
+                  <img
+                    src={`${STORAGE_URL_XS}/${info[value].src}`}
+                    alt={`${info[value].src.split('.')[0]}`}
+                    width={imageRatio > 1 ? '100%' : 'auto'}
+                    height={imageRatio > 1 ? 'auto' : '100%'}
+                    onClick={() => handleMove(value)}
+                    onKeyDown={(e) => {
+                      if (e.keyCode === 13) {
+                        handleMove(value);
+                      }
+                    }}
+                    className="listImage"
+                    style={{ borderRadius: info[value].src === '2013_5.gif' ? 999 : 2 }}
+                  />
+                )}
+              </div>
+            )}
+          </div>
+        );
+      })}
+      {/* </InfiniteScroll> */}
     </div>
   );
 }
