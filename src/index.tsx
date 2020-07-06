a214c25c5eeb7d15f0a123bd3bc0205c5c516a34
Kay Kim <kimjh@bawi.org>
2020-07-06 21:22:05 +0900

    add gtm module

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import smoothscroll from 'smoothscroll-polyfill';
+import TagManager from 'react-gtm-module';
 
 import './index.scss';
 import AppRouter from './AppRouter';
 
 // kick off the polyfill!
 smoothscroll.polyfill();
+
+const tagManagerArgs = {
+  gtmId: 'GTM-N3JGQXG',
+};
+
+TagManager.initialize(tagManagerArgs);
 
 ReactDOM.render(
   <React.StrictMode>
