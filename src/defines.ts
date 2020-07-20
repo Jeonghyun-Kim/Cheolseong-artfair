08cf12f2dc6a9e1fd821e5486f93722144fda4a9
Kay Kim <kimjh@bawi.org>
2020-07-20 17:45:47 +0900

    .

--- src/defines.ts
+++ src/defines.ts
@@ -1,5 +1,6 @@
 export default {
   API_URL: 'https://api.airygall.com',
+  DOMAIN: 'https://ondisplay.co.kr',
   STORAGE_URL_MD: 'https://d3upf6md31d3of.cloudfront.net',
   STORAGE_URL_SM: 'https://d1mqeykb8ywbm3.cloudfront.net',
   STORAGE_URL_XS: 'https://dly1k4se6h02w.cloudfront.net',
