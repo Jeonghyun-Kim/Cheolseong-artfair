bf17568143579d9818442264ce6f2ccb2d0881c1
Kay Kim <kimjh@bawi.org>
2020-07-20 17:43:52 +0900

    remove useless

--- src/defines.ts
+++ src/defines.ts
@@ -1,6 +1,5 @@
 export default {
   API_URL: 'https://api.airygall.com',
-  DOMAIN: 'https://kay.airygall.com',
   STORAGE_URL_MD: 'https://d3upf6md31d3of.cloudfront.net',
   STORAGE_URL_SM: 'https://d1mqeykb8ywbm3.cloudfront.net',
   STORAGE_URL_XS: 'https://dly1k4se6h02w.cloudfront.net',
